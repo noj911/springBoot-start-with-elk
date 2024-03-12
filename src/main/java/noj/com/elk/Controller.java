052f5eb2ada98164c5569ca168abaeea880f701f
emilebruno <brunothiakane91@gmail.com>
2024-03-12 13:32:46 +0000

    logs elk

---
+++ src/main/java/noj/com/elk/Controller.java
@@ -0,0 +1,19 @@
+package noj.com.elk;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Controller {
+
+    private static final Logger LOG = LogManager.getLogger(Controller.class);
+
+    @GetMapping(value = "/echo")
+    public String echoMessage() {
+        LOG.log(Level.INFO, "Echo Triggered");
+        return "Echo Triggered";
+    }
+}
